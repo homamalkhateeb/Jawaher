eb3baf74e67a3ce2937160a5f8d60cb92b3f33bf
homam <homamalkhateeb@gmail.com>
2026-09-07 14:19:26 +0300

    Fix media file settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -137,8 +137,8 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-MIDIA_URL = '/medai/'
-MIDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
